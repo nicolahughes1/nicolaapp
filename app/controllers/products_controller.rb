9a0cdd28b527d3fd71229962ddf4ec725e5ab78a
Nicola <nicola_hughes@icloud.com>
2016-02-03 15:20:11 -0600

    added color field, price field, image

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -67,8 +67,8 @@ class ProductsController < ApplicationController
       @product = Product.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Never trust parameters from the scary internet, only allow the white list through
     def product_params
-      params.require(:product).permit(:name, :description, :image_url)
-    end
+      params.require(:product).permit(:name, :description, :image_url, :colour, :price)
+  end
 end
