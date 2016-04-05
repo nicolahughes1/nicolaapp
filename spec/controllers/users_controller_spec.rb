5b4e6d00f7ec8b031c6611689594c594df7fa43d
Nicola <nicola_hughes@icloud.com>
2016-04-04 21:00:03 -0500

    functional tests

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe UsersController, :type => :controller do
+
+  before do
+    @user = User.create!(email: "test5@test5.com", password: "test5555")
+    user2 = User.create!(email: "test6@test6.com", password: "test6666")
+  end
+
+  describe "GET #show" do
+     context "User is logged in" do
+      before do
+        sign_in @user
+
+     end
+
+      it "loads the correct user details" do
+         get :show, id: @user.id
+         expect(response.status).to eq 200
+         expect(assigns(:user)).to eq @user
+       end
+
+      it "doesn't load the second user" do
+         get :show, id: @user2.id
+         expect(response.status).to eq 302
+         expect(response).to redirect_to(root_path)
+       end
+     end
+
+     context "No user is logged in" do
+       it "redirects to login" do
+         get :show, id: @user.id
+         expect(response).to redirect_to(root_path)
+       end
+     end
+  end
+
+end
