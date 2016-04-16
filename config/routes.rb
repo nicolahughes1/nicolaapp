334ecd3b791814475839fd8c6f959ed5a29579fe
Nicola <nicola_hughes@icloud.com>
2016-04-15 22:18:51 -0500

    payments controller stripe checkout form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   get 'static_pages/contact'
   post 'static_pages/thank_you'
+  post 'payments/create'
 
 devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}, :controllers => { :registrations => "user_registrations"}
 resources :users
